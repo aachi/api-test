8caeace2f5632b4c930a2114d6f0c487f023810c
aachi <asifmanzoorawan@gmail.com>
2019-12-28 09:49:52 +0500

    hello

--- main.go
+++ main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 )
 
 type server struct{}
 
+// Testing for github sync
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusOK)
-    w.Header().Set("Content-Type", "application/json")
-    w.Write([]byte(`{"message": "hello world"}`))
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"message": "hello world"}`))
 }
 
 func main() {
-    s := &server{}
-    http.Handle("/", s)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	s := &server{}
+	http.Handle("/", s)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
